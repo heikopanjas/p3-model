bbc5baabff99ff206dc8f58228b870b6816115d3
Heiko Panjas <heiko@panjas.com>
2025-08-20 22:52:22 +0200

    refactor(core): simplify utility structs to data-focused design
    - Convert all classes to structs for better data model representation
    - Remove all constructors, destructors, and operators from utility structs
    - Simplify String, Guid, Timestamp, and Timespan to placeholder data containers
    - Add comprehensive PlantUML documentation with monochrome styling
    - Integrate Doxygen documentation generation with LaTeX/PDF output
    - Add PlantUML monochrome theme for consistent professional styling
    - Update project documentation and build instructions
    - Add comprehensive CMake documentation targets with PlantUML support

--- p3-model.h
+++ p3-model.h
@@ -1,16 +1,85 @@
+/*!
+ * \file p3-model.h
+ * \brief P3 Model Library Header
+ * \details Main header file for the Podcast Production and Publishing (P3) Model library
+ *
+ * \startuml
+ * !include docs/plantuml-monochrome.puml
+ * title P3 Model Utility Structs
+ *
+ * struct String {
+ *   ' Data members placeholder
+ * }
+ *
+ * struct Guid {
+ *   ' Data members placeholder
+ * }
+ *
+ * struct Timestamp {
+ *   ' Data members placeholder
+ * }
+ *
+ * struct Timespan {
+ *   ' Data members placeholder
+ * }
+ *
+ * struct Model {
+ *   +{static} GetVersion() : string
+ *   +{static} GetLibraryName() : string
+ *   +{static} Initialize() : bool
+ *   +{static} Shutdown() : void
+ * }
+ *
+ * note right of Model : Main library entry point
+ * note top of String : Utility string struct
+ * note top of Guid : Globally unique identifier
+ * note top of Timestamp : Point in time representation
+ * note top of Timespan : Duration representation
+ *
+ * \enduml
+ */
+
 #ifndef __P3_MODEL_H_INCL__
 #define __P3_MODEL_H_INCL__
 
 #pragma pack(push, 8)
 
+#include <cstdint>
 #include <string>
 
 namespace ultralove::p3::model {
-/// \brief Main entry point for the P3 Model library
-/// \details This class provides basic information about the P3 Model library
-class Model
+/// \brief Utility string struct for the P3 Model library
+/// \details Placeholder string struct for future implementation
+struct String
 {
-public:
+    // Data members will be added in future implementation
+};
+
+/// \brief Globally unique identifier struct for the P3 Model library
+/// \details Placeholder GUID struct for future implementation
+struct Guid
+{
+    // Data members will be added in future implementation
+};
+
+/// \brief Timestamp struct for the P3 Model library
+/// \details Placeholder timestamp struct for future implementation
+struct Timestamp
+{
+    // Data members will be added in future implementation
+};
+
+/// \brief Timespan struct for the P3 Model library
+/// \details Placeholder timespan struct for future implementation
+struct Timespan
+{
+    // Data members will be added in future implementation
+};
+
+/// \brief Main entry point for the P3 Model library
+/// \details This struct provides basic information about the P3 Model library
+struct Model
+{
     /// \brief Get the library version
     /// \return Version string in format "major.minor.patch"
     static std::string GetVersion();
@@ -26,8 +95,7 @@ public:
     /// \brief Cleanup and shutdown the P3 Model library
     static void Shutdown();
 
-private:
-    // Private constructor - this is a utility class
+    // Deleted constructors and assignment operators - this is a utility struct
     Model()                        = delete;
     virtual ~Model()               = delete;
     Model(const Model&)            = delete;
